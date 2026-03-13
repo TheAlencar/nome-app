bc1f167df7cb76470408f35503312ded417eb56d
Guilherme Alencar <guilhrermealencar@gmail.com>
2026-03-12 21:58:07 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import Saudacao from './components/Saudacao'
 
-const nome = 'Guilherme'
 
 const logado = 'true'
 
 function App() {
 
+const [nome, setNome] = useState ('Guilherme')
 
   return (
     <>
       <h1>Meu App React</h1>
-      <p className='paragrafo' color='blue'>Utilizando o vite</p>
+      <p className='paragrafo'>Utilizando o vite</p>
 
 
       <h3>Olá meu nome é {nome}</h3>
@@ -19,10 +20,15 @@ function App() {
       <h2>Idade: {10+14}</h2>
         
       <Saudacao
-        logado={logado} />
+        logado={logado}
+        />  
 
-        <Saudacao
-        logado={logado} />
+        <button onClick={
+         () => {
+         console.log ('Nome alterado')
+         setNome(nome === 'Guilherme' ? 'Guilherme Alencar' : 'Guilherme');  
+        }
+      }>Mudar o nome com useState</button>
     </>
   )
 }
